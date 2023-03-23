83835fcfb373385dd32fe34e91e0d6cb42ac34a8
Lubov_Pisarevskaya <lyubo_03@internet.ru>
2023-03-23 18:26:21 +0300

    Color=>white

--- Kursach/main.cpp
+++ Kursach/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    shape.setFillColor(sf::Color::White);
 
     while (window.isOpen())
     {
