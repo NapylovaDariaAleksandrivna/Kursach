46d0a4768ca4f476f882391fd53cf0e9fc65c489
DinoFF <124875138+NapylovaDariaAleksandrivna@users.noreply.github.com>
2023-04-09 15:36:25 +0300

    Add files via upload

---
+++ parser.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#pragma once
+
+
+
+class parser {
+private:
+	std::string input;
+	std::string output;
+public:
+	void setIn(std::string& input) { this->input = input; }
+	void setOut(std::string& output) { this->output = output; }
+	std::string getIn()const { return input; }
+	std::string getOut()const { return output; }
+	parser(std::string in) :input(in){};
+	parser() {};
+	void print(std::string pref, double x1=-5, double x2=5, double y1=-5, double y2=5, double dep = 1);
+};
+std::ostream& operator<< (std::ostream& out, const parser& obj);
+std::istream& operator>> (std::istream& in,  parser& obj);
